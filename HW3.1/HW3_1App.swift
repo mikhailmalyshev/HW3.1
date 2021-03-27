df1f2b421af332ed96267a25199376c1cc288459
Mikhail Malyshev <malyshev090994@gmail.com>
2021-03-27 12:18:51 +0300

    Initial Commit

---
+++ HW3.1/HW3_1App.swift
@@ -0,0 +1,17 @@
+//
+//  HW3_1App.swift
+//  HW3.1
+//
+//  Created by Михаил Малышев on 27.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct HW3_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
